9a615a6de1171f23d83beb7adfb2821266cf1a46
Lex Tang <lexrus@gmail.com>
2012-08-15 15:19:40 +0800

    Perform scrollViewDidScroll delegate.

--- PunchScrollView.m
+++ PunchScrollView.m
@@ -520,6 +520,10 @@
         [self pageIndexChanged];
     }
     
+    if ([self.delegate respondsToSelector:@selector(scrollViewDidScroll:)])
+    {
+        [self.delegate performSelector:@selector(scrollViewDidScroll:) withObject:self];
+    }
 }
 
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
