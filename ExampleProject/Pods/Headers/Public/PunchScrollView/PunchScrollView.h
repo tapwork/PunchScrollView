bf343192e3ff17be09853c4abce539322db815a9
Christian Menschel <christian@tapwork.de>
2014-11-21 20:43:40 +0100

    Example projects are running cocoapods now

---
+++ ExampleProject/Pods/Headers/Public/PunchScrollView/PunchScrollView.h
@@ -0,0 +1,1 @@
+../../../../../PunchScrollView.h
---
+++ ImageExample/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// PunchScrollView
+#define COCOAPODS_POD_AVAILABLE_PunchScrollView
+#define COCOAPODS_VERSION_MAJOR_PunchScrollView 1
+#define COCOAPODS_VERSION_MINOR_PunchScrollView 1
+#define COCOAPODS_VERSION_PATCH_PunchScrollView 0
+
